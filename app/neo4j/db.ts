5ccb6dc877304eeac7968e563adac15759911278
Rob Brennan <rob@therobbrennan.com>
2021-01-20 20:55:19 -0800

    Remove DEFAULT_NEO4J_DESKTOP constant

--- app/neo4j/db.ts
+++ app/neo4j/db.ts
@@ -1,13 +1,5 @@
 import * as neo4j from "neo4j-driver"
 
-// Neo4j Desktop settings
-export const DEFAULT_NEO4J_DESKTOP = {
-  URI: "bolt://localhost:7687",
-  USER: "neo4j",
-  PASSWORD: "neo4j",
-  isEncrypted: false, // v3.5 does not use encrypted connections, but 4.0 does
-}
-
 // Create a configured neo4j driver instance (this doesn't start a session)
 export const driver = (
   neo4jURI = process.env.NEO4J_URI,
