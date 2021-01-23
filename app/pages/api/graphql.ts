bf557123869a03ab8781ab49c1c98dffd60a2f5f
Rob Brennan <rob@therobbrennan.com>
2021-01-23 15:26:13 -0800

    Do not allow GraphQL introspection or the GraphIQL playground if we are deployed on Vercel

--- app/pages/api/graphql.ts
+++ app/pages/api/graphql.ts
@@ -2,6 +2,14 @@ import { ApolloServer } from "apollo-server-micro"
 import { driver } from "../../neo4j/db"
 import { augmentedSchema } from "../../apollo/schema"
 
+// Use Vercel system environment variables to determine if we should allow introspection and the GraphIQL playground
+// See https://vercel.com/docs/environment-variables#system-environment-variables for more details
+export const RUNNING_ON_VERCEL = !!process.env.VERCEL_ENV
+
+// If we are deployed to Vercel, disable introspection and the GraphIQL playground for our GraphQL endpoint
+export const ALLOW_GRAPHQL_INTROSPECTION = !RUNNING_ON_VERCEL ? true : false
+export const ALLOW_GRAPHQL_PLAYGROUND = !RUNNING_ON_VERCEL ? true : false
+
 export const neo4jDriverInstance = driver()
 
 export const apolloServer = new ApolloServer({
@@ -9,8 +17,8 @@ export const apolloServer = new ApolloServer({
   context: ({ req }) => ({ req, driver: neo4jDriverInstance }),
 
   // Disable GraphIQL in production by setting these to false
-  introspection: true,
-  playground: true,
+  introspection: ALLOW_GRAPHQL_INTROSPECTION,
+  playground: ALLOW_GRAPHQL_PLAYGROUND,
 })
 
 // We need to disable the bodyParser so we can consume our API endpoint as a stream
