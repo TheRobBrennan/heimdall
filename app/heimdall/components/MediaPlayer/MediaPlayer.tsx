069eb82204d2dbbf60d364b369807ec6b394426a
Rob Brennan <rob@therobbrennan.com>
2021-01-23 14:49:24 -0800

    Use local demo video and closed captioning tracks

--- app/heimdall/components/MediaPlayer/MediaPlayer.tsx
+++ app/heimdall/components/MediaPlayer/MediaPlayer.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useEffect } from "react"
 import Plyr from "plyr"
 
 export const MediaPlayer = () => {
@@ -13,40 +13,49 @@ export const MediaPlayer = () => {
           // @ts-ignore Plyr property 'crossorigin' does not exist on type 'DetailedHTMLProps<VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>'
           crossorigin
           playsinline
-          poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+          poster="/__demo__/View_From_A_Blue_Moon_Trailer-HD.jpg"
         >
           <source
-            src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+            src="/__demo__/View_From_A_Blue_Moon_Trailer-576p.mp4"
             type="video/mp4"
             // @ts-ignore Plyr property 'size' does not exist on type 'DetailedHTMLProps<SourceHTMLAttributes<HTMLSourceElement>, HTMLSourceElement>'
             size="576"
           />
-          <source
-            src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
+          {/**
+           * Here are examples of how additional video files might look.
+           * GitHub has a maximum file size limit of 100 MB per file, and will refuse git commit pushes that are more than 100 MB in total size.
+           * This repository will store reference video at the low 576p resolution.
+           *
+           * Feel free to download the additional videos for your local project at:
+           *  720p  - https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4
+           *  1080p - https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4
+           */}
+          {/* <source
+            src="/__demo__/View_From_A_Blue_Moon_Trailer-720p.mp4"
             type="video/mp4"
             // @ts-ignore Plyr property 'size' does not exist on type 'DetailedHTMLProps<SourceHTMLAttributes<HTMLSourceElement>, HTMLSourceElement>'
             size="720"
-          />
-          <source
-            src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
+          /> */}
+          {/* <source
+            src="/__demo__/View_From_A_Blue_Moon_Trailer-1080p.mp4"
             type="video/mp4"
             // @ts-ignore Plyr property 'size' does not exist on type 'DetailedHTMLProps<SourceHTMLAttributes<HTMLSourceElement>, HTMLSourceElement>'
             size="1080"
-          />
+          /> */}
 
           {/* Captions */}
           <track
             kind="captions"
             label="English"
             srcLang="en"
-            src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
+            src="/__demo__/captions/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
             default
           />
           <track
             kind="captions"
             label="Français"
             srcLang="fr"
-            src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"
+            src="/__demo__/captions/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"
           />
 
           {/* Fallback for browsers that don't support the <video> tag */}
