0dd078506cdff36dd82f5906fa90de6517456865
Rob Brennan <rob@therobbrennan.com>
2021-01-23 21:37:42 -0800

    Update MediaPlayer tests

--- app/heimdall/components/MediaPlayer/MediaPlayer.test.tsx
+++ app/heimdall/components/MediaPlayer/MediaPlayer.test.tsx
@@ -1,12 +1,19 @@
-import React from "react"
-import TestRenderer from "react-test-renderer"
+import * as React from "react"
+import { cleanup, render } from "@testing-library/react"
+import { MockedProvider } from "@apollo/client/testing"
 
 import Component from "./MediaPlayer"
 
-describe("The MediaPlayer component", () => {
-  it(`should render`, () => {
-    // Verify success state
-    const component = TestRenderer.create(<Component />)
-    expect(component.toJSON()).toMatchSnapshot()
+describe("Our MediaPlayer component", () => {
+  afterEach(cleanup)
+
+  it("should render without children", () => {
+    const { asFragment } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Component />
+      </MockedProvider>
+    )
+
+    expect(asFragment).toMatchSnapshot()
   })
 })
