8f4639753ace1ed3d5c46b18932a7fc99e8271a4
Rob Brennan <rob@therobbrennan.com>
2021-01-23 22:11:46 -0800

    Enhance Debug component tests

--- app/heimdall/components/Debug/Debug.test.tsx
+++ app/heimdall/components/Debug/Debug.test.tsx
@@ -1,33 +1,37 @@
-import React from "react"
-import TestRenderer from "react-test-renderer"
+import * as React from "react"
+import { cleanup, render } from "@testing-library/react"
 import { MockedProvider } from "@apollo/client/testing"
 
-import Component from "./Debug"
+import Component, { IDebugProps as Props } from "./Debug"
 
 import type { Auth0User } from "../../../auth0/user"
 
 describe("Our Debug component", () => {
-  describe("should render", () => {
-    it("Auth0 user details", () => {
-      const user: Auth0User = {
-        given_name: "Rob",
-        family_name: "Brennan",
-        nickname: "rob",
-        name: "Rob Brennan",
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GijQDyp8j2v_8Dg3WMs7BtVIqs-dDVizUnEuNLbPKI=s96-c",
-        gender: "male",
-        locale: "en",
-        updated_at: "2021-01-24T04:46:38.864Z",
-        sub: "google-oauth2|116058668302290861810",
-      }
+  afterEach(cleanup)
 
-      const component = TestRenderer.create(
-        <MockedProvider mocks={[]} addTypename={false}>
-          <Component user={user} />
-        </MockedProvider>
-      )
-      expect(component.toJSON()).toMatchSnapshot()
-    })
+  it("should render Auth0 user details", () => {
+    const user: Auth0User = {
+      given_name: "Rob",
+      family_name: "Brennan",
+      nickname: "rob",
+      name: "Rob Brennan",
+      picture:
+        "https://lh3.googleusercontent.com/a-/AOh14GijQDyp8j2v_8Dg3WMs7BtVIqs-dDVizUnEuNLbPKI=s96-c",
+      gender: "male",
+      locale: "en",
+      updated_at: "2021-01-24T04:46:38.864Z",
+      sub: "google-oauth2|116058668302290861810",
+    }
+    const props: Props = {
+      user,
+    }
+
+    const { asFragment } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Component {...props} />
+      </MockedProvider>
+    )
+
+    expect(asFragment).toMatchSnapshot()
   })
 })
