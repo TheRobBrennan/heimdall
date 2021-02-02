a9869b4f978033e0a70637478476673976b82417
Rob Brennan <rob@therobbrennan.com>
2021-02-01 23:22:51 -0800

    Move to schema folder

--- app/graphql/schema/schema.ts
+++ app/graphql/schema/schema.ts
@@ -1,6 +1,6 @@
 import { makeAugmentedSchema } from "@neo4j/graphql"
-import { typeDefs } from "./type-definitions/type-defs"
-import { resolvers } from "./resolvers/resolvers"
+import { typeDefs } from "../type-definitions/type-defs"
+import { resolvers } from "../resolvers/resolvers"
 
 export const augmentedSchema = makeAugmentedSchema({
   typeDefs,
--- app/graphql/type-definitions/type-defs.ts
+++ app/graphql/type-definitions/type-defs.ts
@@ -2,5 +2,5 @@ import fs from "fs"
 import path from "path"
 
 export const typeDefs = fs
-  .readFileSync(path.resolve(`graphql/schema.graphql`))
+  .readFileSync(path.resolve(`graphql/schema/schema.graphql`))
   .toString("utf-8")
